da229a43e05dbc384e9b61ed2ff267490dd9cee4
s2iwi2s <48598006+s2iwi2s@users.noreply.github.com>
2020-09-10 02:18:28 -0500

    Update HeaderComponent.jsx

--- ui/react/src/forms/common/HeaderComponent.jsx
+++ ui/react/src/forms/common/HeaderComponent.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
 import { withRouter } from 'react-router';
+import { Link } from 'react-router-dom';
 
 class HeaderComponent extends React.Component {
     render = () => {
         return (
             <header className="bg-dark">
-                <nav className="container navbar navbar-expand-md navbar-dark">
+                <nav className="navbar navbar-expand-md navbar-dark bg-dark">
                     <div><a className="navbar-brand" href="/">My React Crude App</a></div>
+
+                    <ul className="navbar-nav">
+                        <li key="my-case-list"><Link className="nav-link" to="/my-case-list">My Case List</Link></li>
+                        <li key="part-item-list"><Link className="nav-link" to="/part-item-list">Part Item list</Link></li>
+                    </ul>
                 </nav>
             </header>
         );
